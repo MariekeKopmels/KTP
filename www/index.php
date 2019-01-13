f20ae843f1b17c2bc8f422019e2980f1bb50c48a
Maureen <m.vandergrinten@live.nl>
2019-01-13 11:21:25 +0100

    front page

--- www/index.php
+++ www/index.php
@@ -8,25 +8,23 @@ date_default_timezone_set('Europe/Amsterdam');
 
 $errors = array();
 
-// if ($_SERVER['REQUEST_METHOD'] == 'POST'
-// 	&& isset($_FILES['knowledgebase'])
-// 	&& $file = process_file($_FILES['knowledgebase'], $errors))
-// {
-// 	switch ($_POST['action'])
-// 	{
-// 		case 'analyse':
-// 			header('Location: analyse.php?kb=' . rawurlencode($file));
-// 			break;
+if ($_SERVER['REQUEST_METHOD'] == 'POST')
+{
+	switch ($_POST['action'])
+	{
+		case 'analyse':
+			header('Location: analyse.php?kb=' . rawurlencode($file));
+			break;
 
-// 		case 'run':
-// 			header('Location: webfrontend.php?kb=' . rawurlencode($file));
-// 			break;
-// 	}
+		case 'run':
+			header('Location: webfrontend.php?kb=SheepDiagnosis.xml');
+			break;
+	}
 
-// 	exit;
-// }
+	exit;
+}
 
-header('Location: webfrontend.php?kb=SheepDiagnosis.xml');
+//header('Location: webfrontend.php?kb=SheepDiagnosis.xml');
 
 function process_file($file, array &$errors = array())
 {
@@ -50,7 +48,7 @@ function process_file($file, array &$errors = array())
 	return $unique_name;
 }
 
-$template = new Template('templates/single.phtml');
+$template = new Template('templates/start.phtml');
 $template->errors = $errors;
 
 echo $template->render();
